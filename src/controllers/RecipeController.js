8edda582ee2399d903170b1a3004e9e1457d8d05
Juan Cruz Lima Caramico <juan.mdq22@gmail.com>
2023-05-03 01:59:29 -0300

    deploy

--- src/controllers/RecipeController.js
+++ src/controllers/RecipeController.js
@@ -6,82 +6,92 @@ require("dotenv").config();
 const API_KEY = process.env.API_KEY;
 
 exports.getAllRecipes = async () => {
-  const dbResults = await Recipe.findAll({
-    include: {
-      model: Diet,
-      attributes: ["name"],
-      through: {
-        attributes: [],
+  try {
+    
+    const dbResults = await Recipe.findAll({
+      include: {
+        model: Diet,
+        attributes: ["name"],
+        through: {
+          attributes: [],
+        },
       },
-    },
-  });
-  const dbRes = dbResults.map((recipe) => {
-    return {
-      id: recipe.id,
-      title: recipe.title,
-      summary: recipe.summary,
-      healthScore: recipe.healthScore,
-      image: recipe.image,
-      diets: recipe.Diets.map((diet) => diet.name),
-      steps: JSON.parse(recipe.steps),
-      source: "db"
-    };
-  });
+    });
+    const dbRes = dbResults.map((recipe) => {
+      return {
+        id: recipe.id,
+        title: recipe.title,
+        summary: recipe.summary,
+        healthScore: recipe.healthScore,
+        image: recipe.image,
+        diets: recipe.Diets.map((diet) => diet.name),
+        steps: JSON.parse(recipe.steps),
+        source: "db"
+      };
+    });
+  
+    const { data } = await axios.get(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=100&addRecipeInformation=true`
+    );
+    const { results } = data;
+    const apiResults = results.map((recipe) => {
+      return {
+        id: recipe.id,
+        title: recipe.title,
+        summary: recipe.summary,
+        healthScore: recipe.healthScore,
+        image: recipe.image,
+        diets: recipe.diets,
+        steps: recipe.analyzedInstructions[0]?.steps.map((e) => {
+          return {
+            number: e.number,
+            step: e.step,
+          };
+        }),
+        source: "api"
+      };
+    });
+  
+    return [...dbRes, ...apiResults];
+  } catch (error) {
+    throw Error(error.message)
+  }
+};
 
-  const { data } = await axios.get(
-    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=100&addRecipeInformation=true`
-  );
-  const { results } = data;
-  const apiResults = results.map((recipe) => {
+exports.getRecipeFromAPI = async (idRecipe) => {
+  try {
+    
+    const { data } = await axios.get(
+      `https://api.spoonacular.com/recipes/${idRecipe}/information?apiKey=${API_KEY}`
+    );
+    const {
+      id,
+      title,
+      summary,
+      healthScore,
+      analyzedInstructions,
+      image,
+      diets,
+    } = data;
+  
     return {
-      id: recipe.id,
-      title: recipe.title,
-      summary: recipe.summary,
-      healthScore: recipe.healthScore,
-      image: recipe.image,
-      diets: recipe.diets,
-      steps: recipe.analyzedInstructions[0]?.steps.map((e) => {
+      id,
+      title,
+      summary,
+      healthScore,
+      steps: analyzedInstructions[0]?.steps.map((e) => {
         return {
           number: e.number,
           step: e.step,
         };
       }),
+      image,
+      diets,
       source: "api"
     };
-  });
-
-  return [...dbRes, ...apiResults];
-};
-
-exports.getRecipeFromAPI = async (idRecipe) => {
-  const { data } = await axios.get(
-    `https://api.spoonacular.com/recipes/${idRecipe}/information?apiKey=${API_KEY}`
-  );
-  const {
-    id,
-    title,
-    summary,
-    healthScore,
-    analyzedInstructions,
-    image,
-    diets,
-  } = data;
-
-  return {
-    id,
-    title,
-    summary,
-    healthScore,
-    steps: analyzedInstructions[0]?.steps.map((e) => {
-      return {
-        number: e.number,
-        step: e.step,
-      };
-    }),
-    image,
-    diets,
-    source: "api"
-  };
+  } catch (error) {
+    throw Error(error.message)
+  }
 };
 exports.getRecipeFromDB = async (idRecipe) => {
   try {
